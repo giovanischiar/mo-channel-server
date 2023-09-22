af8abbcd87fb5186264e7dcddc8d2c80e3eb06ff
Giovani Schiar <giovanischiar@gmail.com>
2023-09-22 16:01:19 -0700

    Create a Route That Read All Files Given a Directory, and  Return a JSON of File Names  With Each URL

--- app.js
+++ app.js
@@ -1,11 +1,59 @@
+const config = require('./config.json')
 const express = require('express')
+const fs = require('fs')
 const app = express()
-const port = 3000
+const port = config.port
 
 app.get('/teste', (req, res) => {
-  res.send('hello, world')
+	let files = []
+	let root = config.root
+	let filesPort = config.fileServerPort
+	getFiles(root, files)
+	let onlyVideoFiles = files.filter(word => word.includes(".mkv") || word.includes(".avi"))
+	let jsonPrepare = onlyVideoFiles.map( filePath => {
+			let filePathWithoutRoot = filePath.replace(root, "")
+			return {
+				name: filePathWithoutRoot,
+				url: `http://${req.connection.localAddress.replace("::ffff:", "")}:${filesPort}${filePathWithoutRoot}`
+			}
+		}
+	)
+
+	let result = {}
+
+	jsonPrepare.forEach(elem => {
+		let key = elem.name.split("/")[1]
+		let newElem = elem
+		newElem.name = newElem.name.replace(`/${key}/`, "")
+		if (result[key] == undefined) {
+			result[key] = [newElem]
+		} else {
+			result[key].push(newElem)
+		}
+	})
+
+	res.send(result)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+	console.log(`Example app listening on port ${port}`)
 })
+
+// Recursive function to get files
+function getFiles(dir, files = []) {
+	// Get an array of all files and directories in the passed directory using fs.readdirSync
+	const fileList = fs.readdirSync(dir)
+	// Create the full path of the file/directory by concatenating the passed directory and file/directory name
+	for (const file of fileList) {
+		const name = `${dir}/${file}`
+		// Check if the current file/directory is a directory using fs.statSync
+		if (fs.statSync(name).isDirectory()) {
+			// If it is a directory, recursively call the getFiles function with the directory path and the files array
+			getFiles(name, files)
+		} else {
+			// If it is a file, push the full path to the files array
+			files.push(name)
+		}
+	}
+	return files
+}
